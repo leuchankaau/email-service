a4c38f470df6bdcce14eb8cbddb88f52de8b3cc3
leuchankaau <Git2020!>
2020-01-10 22:36:23 +1100

    fix response for get email status

--- emailStatus/index.js
+++ emailStatus/index.js
@@ -3,31 +3,41 @@ const docClient = new AWS.DynamoDB.DocumentClient();
 const TABLE_NAME = 'emails';
 
 exports.handler = function (event, context, callback) {
-  console.log("event", event);
-  console.log("event.queryStringParameters.uuid", event.queryStringParameters.uuid);
-  var params = {
-    TableName: TABLE_NAME,
-    Key: {
-      "uuid": event.queryStringParameters.uuid
-    }
-  };
+    console.log("event", event);
+    console.log("event.queryStringParameters.uuid", event.queryStringParameters.uuid);
+    const params = {
+        TableName: TABLE_NAME,
+        Key: {
+            "uuid": event.queryStringParameters.uuid
+        }
+    };
 
-  console.log("Call DynamoDB.");
-  // Call DynamoDB to read the item from the table
-  docClient.get(params, function (err, data) {
-    if (err) {
-      console.log("err", err);
-      callback(err, null);
-    } else {
-      console.log("data", data);
-      callback(null, {
-        uuid: data.Item.uuid,
-        provider: data.Item.provider,
-        statusCode: data.Item.statusCode,
-        state: data.Item.state,
-        errors: data.Item.errors
-      });
-    }
-  });
+
+    console.log("Call DynamoDB.");
+// Call DynamoDB to read the item from the table
+    docClient.get(params, function (err, data) {
+        if (err) {
+            console.log("err", err);
+            const response = {
+                "statusCode": 500,
+                "body": JSON.stringify(err),
+                "isBase64Encoded": false
+            };
+            callback(response, null);
+        } else if (data) {
+            console.log("data", data);
+
+            const response = {
+                "statusCode": 200,
+                "body": JSON.stringify({
+                    uuid: data.Item.uuid, provider: data.Item.provider, statusCode: data.Item.statusCode,
+                    state: data.Item.state,
+                    errors: data.Item.errors
+                }),
+                "isBase64Encoded": false
+            };
+            callback(null, response);
+        }
+    });
 
 };
