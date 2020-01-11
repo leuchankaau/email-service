ea1c7970f5c4518205fcd8f75b2298b955894bcd
leuchankaau <Git2020!>
2020-01-11 11:04:00 +1100

    fix get issue

--- sendGridAdapter/index.js
+++ sendGridAdapter/index.js
@@ -2,90 +2,84 @@ const util = require('./util');
 const https = require('https');
 
 exports.handler = async (records) => {
-  const mes = JSON.stringify(records);
-  for (const message of records.Records) {
-  return processMessage(JSON.parse(message.body));
-  }
+    for (const message of records.Records) {
+        await processMessage(JSON.parse(message.body));
+    }
 };
 
 function processMessage(event) {
-  const auth = 'Bearer ' + process.env.SENDGRID_API_KEY;
-  const msg = buildPayload(event);
-  const body = JSON.stringify(msg);
-  console.log('body:', body);
-  return new Promise((resolve, reject) => {
-    const options = {
-      host: 'api.sendgrid.com',
-      path: '/v3/mail/send',
-      port: 443,
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'Content-Length': body.length,
-        'Authorization': auth
-      }
-    };
-    const req = https.request(options, (res) => {
-      console.log('res statusCode:', res.statusCode);
-      if (res.statusCode < 200 || res.statusCode > 299) {
-        res.on('data', function (errorData) {
-          console.log('errorData: ' + errorData);
-          if (res.statusCode == 400) { //if Bad Request save it to Dynamo DB
-            event.errors = JSON.parse(errorData);
-            event.state = util.BAD_REQUEST;
-            event.statusCode = res.statusCode;
+    const auth = 'Bearer ' + process.env.SENDGRID_API_KEY;
+    const msg = buildPayload(event);
+    const body = JSON.stringify(msg);
+    console.log('body:', body);
+    return new Promise((resolve, reject) => {
+        const options = {
+            host: 'api.sendgrid.com',
+            path: '/v3/mail/send',
+            port: 443,
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Content-Length': body.length,
+                'Authorization': auth
+            }
+        };
+        const req = https.request(options, (res) => {
+            console.log('res statusCode:', res.statusCode);
+            if (res.statusCode < 200 || res.statusCode > 299) {
+                res.on('data', function (resData) {
+                    console.log('resData: ' + resData);
+                    if (res.statusCode === 400) { //if Bad Request save it to Dynamo DB
+                        event.errors = JSON.parse(resData);
+                        event.state = util.BAD_REQUEST;
+                        event.statusCode = res.statusCode;
+                    } else {
+                        reject(resData);
+                    }
+                });
+            } else {
+                event.state = util.SUCCESS;
+            }
             util.saveItemDB(event);
-            resolve('Success');
-          }
-          else {
-            reject(errorData);
-          }
         });
-      }
-      else {
-        event.state = util.SUCCESS;
-        util.saveItemDB(event);
-        resolve('Success');
-      }
+        req.on('error', (e) => {
+            console.log('error:', e);
+            reject(e.message);
+        });
+        // send the request
+        req.write(body);
+        req.end();
     });
-    req.on('error', (e) => {
-      console.log('error:', e);
-      reject(e.message);
-    });
-    // send the request
-    req.write(body);
-    req.end();
-  });
 }
 
 function buildPayload(event) {
-  const toArray = [];
-  buildEmaillArray(event.to, toArray);
-  const ccArray = [];
-  buildEmaillArray(event.cc, ccArray);
-  const bccArray = [];
-  buildEmaillArray(event.bcc, bccArray);
-  const msg = {
-    "personalizations": [{
-      "to": toArray
-    }],
-    "from": {
-      "email": event.from
-    },
-    "subject": event.subject,
-    "content": [{
-      "type": "text/plain",
-      "value": event.body
-    }]
-  };
-  return msg;
+    const toArray = [];
+    buildEmaillArray(event.to, toArray);
+    const ccArray = [];
+    buildEmaillArray(event.cc, ccArray);
+    const bccArray = [];
+    buildEmaillArray(event.bcc, bccArray);
+    return {
+        "personalizations": [{
+            "to": toArray
+        }],
+        "from": {
+            "email": event.from
+        },
+        "subject": event.subject,
+        "content": [{
+            "type": "text/plain",
+            "value": event.body
+        }]
+    };
 }
 
 function buildEmaillArray(inputArray, resultArray) {
-  if(inputArray){
-  for (const email of inputArray) {
-    resultArray.push({
-      "email": email
-    });
-  }}
+    if (inputArray) {
+        for (const email of inputArray) {
+            resultArray.push({
+                "email": email
+            });
+        }
+    }
 }
--- util/util.js
+++ util/util.js
@@ -12,51 +12,49 @@ const sqs = new AWS.SQS();
 
 
 function saveItemDB(event) {
-    return new Promise(resolve => {
-        const params = {
-            TableName: TABLE_NAME,
-            Item: {
-                'uuid': event.uuid,
-                'statusCode': event.statusCode,
-                'state': event.state,
-                'retryCount': event.retryCount,
-                'provider': event.provider,
-                'errors': event.errors,
-                'event': JSON.stringify(event)
-            }
-        };
-
-        ddb.put(params, function (err, data) {
-
-            if (err) {
-                console.log(err);
-                return err;
-            } else {
-                return data;
-            }
-        });
+    console.log('event:', event);
+    const params = {
+        TableName: TABLE_NAME,
+        Item: {
+            'uuid': event.uuid,
+            'statusCode': event.statusCode,
+            'state': event.state,
+            'retryCount': event.retryCount,
+            'provider': event.provider,
+            'errors': event.errors,
+            'event': event
+        }
+    };
+    console.log('params:', params);
+    ddb.put(params, function (err, data) {
+        if (err) {
+            console.log('error:', err);
+        } else {
+            console.log('data:', data);
+        }
     });
+    console.log('DB saved');
 }
 
 function sendMessage(event, queue_url, context) {
-    const response = {uuid:event.uuid};
     const params = {
         MessageBody: JSON.stringify(event),
         MessageGroupId: event.uuid,
         MessageDeduplicationId: event.uuid,
         QueueUrl: queue_url
     };
+    saveItemDB(event);
     sqs.sendMessage(params, function (err, data) {
         if (err) {
             console.log('error:', "Fail Send Message" + err);
             context.done('error', "ERROR Put SQS"); // ERROR with message
         } else {
             console.log('data:', data.MessageId);
-            saveItemDB(event);
-            context.done(null, {uuid: event.uuid,provider:event.provider,state:event.state}); // SUCCESS
+            context.done(null, {uuid: event.uuid, provider: event.provider, state: event.state}); // SUCCESS
         }
     });
 }
+
 module.exports = {
     ACCEPTED: ACCEPTED,
     FAILED: FAILED,
